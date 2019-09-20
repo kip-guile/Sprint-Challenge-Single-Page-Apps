1473fbf08f4c4a7a223ddda2c440caa3e899ddb1
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-20 09:11:29 +0100

    characters rendering

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+export default function CharacterCard(props) {
+  const {image, name, species, status, location} = props;
+
+  return (
+    <div>
+      <div>
+        <img src={image}/>
+      </div>
+
+      <div>
+        <h4>{name}</h4>
+        <p>{species}</p>
+        <p>{status}</p>
+        <p>{location}</p>
+      </div>
+    </div>
+  );
+};
